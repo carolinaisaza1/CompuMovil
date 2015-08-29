e10297ff08d2654fb0066211e715b7b0d20499b1
Jaime <jaime951@gmail.com>
2015-08-28 19:17:54 -0500

    Initial commit

---
+++ Laboratorios/Yamba/app/src/main/java/co/edu/udea/cmovil/gr6/yamba/StatusActivity.java
@@ -0,0 +1,60 @@
+package co.edu.udea.cmovil.gr6.yamba;
+
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+
+
+public class StatusActivity extends Activity implements OnClickListener {
+
+    private static final String TAG = "StatusActivity";
+    private EditText editStatus;
+    private Button buttonTweet;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_status);
+        editStatus = (EditText) findViewById(R.id.hint_status);
+        buttonTweet = (Button) findViewById(R.id.button_tweet);
+        buttonTweet.setOnClickListener(this);
+    }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_status, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        String status= editStatus.getText().toString();
+        Log.d(TAG, "Onclicked with status: " + status);
+    }
+}
